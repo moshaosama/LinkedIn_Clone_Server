b25906638f93e9a609a7238968f49021be6a97d1
moshaosama <mohamedOSFekry@gmail.com>
2025-02-12 23:07:29 +0200

    Latest Version

--- src/main/java/com/example/Linkedin_Clone/About/aboutRepository.java
+++ src/main/java/com/example/Linkedin_Clone/About/aboutRepository.java
@@ -1,6 +1,8 @@
 package com.example.Linkedin_Clone.About;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface aboutRepository extends JpaRepository<About, Integer> {
 }
